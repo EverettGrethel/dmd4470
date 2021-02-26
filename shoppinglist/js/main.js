2b1e5085877dd5df0b39a4fc2bec3215fed79f38
Everett Grethel <everett.grethel@gmail.com>
2021-02-26 01:31:28 -0500

    added checkbox

--- shoppinglist/js/main.js
+++ shoppinglist/js/main.js
@@ -1,5 +1,6 @@
 class Item {
-    constructor(name, aisle, quantity) {
+    constructor(id=null, name, aisle, quantity) {
+    this.id = id;
       this.name = name;
       this.aisle = aisle;
       this.quantity = quantity;
@@ -12,11 +13,14 @@ class Item {
 }
 
 let shoppingList = [];
+let rawData;
 let instances;
 
 db.collection('list').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
+        rawData = snapshot.docs;
         let item = new Item();
+        item.id = doc.id;
         item.name = doc.data().name;
         item.aisle = doc.data().aisle;
         item.quantity = doc.data().quantity;
@@ -36,17 +40,23 @@ let onClose = () => {
     item.aisle = document.getElementById('aisle').value;
     item.quantity = document.getElementById('quantity').value;
     item.addToList();
+    db.collection('list').add({
+        name: item.name,
+        aisle: item.aisle,
+        quantity: item.quantity
+    })
     console.log(item);
 }
 
 function displayShoppingList() {
-    console.log("eh");
+    console.log("display shopping list!");
     // Find the class roster unordered list
     let list = document.getElementById("shoppingList");
     // Clear out the existing list.
     list.innerText = "";
     // For each classmate in the roster, add a list item.
     shoppingList.forEach(item => {
+        let outer_div = document.createElement("div");
         let div = document.createElement("div");
         div.classList.add("item");
 
@@ -70,12 +80,25 @@ function displayShoppingList() {
         button.addEventListener('click', () => {
             let removeIndex = shoppingList.map(function(x) { return x.id; }).indexOf(item.name);
             shoppingList.splice(removeIndex, 1);
+            db.collection('list').doc(item.id).delete();
             displayShoppingList();
-            console.log("hello");
+            console.log(item.id);
         });
         div.appendChild(button);
 
-        list.appendChild(div);
+        let form_checkbox = document.createElement("form");
+        form_checkbox.setAttribute("action", "#");
+        let checkbox = document.createElement("p");
+        form_checkbox.appendChild(checkbox)
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        checkbox.appendChild(label);
+        checkbox.appendChild(input);
+        outer_div.append(form_checkbox);
+        outer_div.append(div);
+
+        list.appendChild(outer_div);
     });
   }
 
@@ -87,3 +110,25 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     instances = M.Modal.init(modal, modalOptions);
   });
+
+//This function checks whether another user has made any changes and updates locally
+function checkDatabase() {
+    db.collection('list').get().then((snapshot) => {
+        shoppingList = [];
+        snapshot.docs.forEach((doc, i) => {
+            if (snapshot.docs[i].name != rawData[i].name) {
+                snapshot.docs.forEach(doc => {
+                    let item = new Item();
+                    item.id = doc.id;
+                    item.name = doc.data().name;
+                    item.aisle = doc.data().aisle;
+                    item.quantity = doc.data().quantity;
+                    shoppingList.push(item);
+                });
+                displayShoppingList();
+            }
+        })
+    });
+}
+
+window.setInterval(checkDatabase, 1000);
