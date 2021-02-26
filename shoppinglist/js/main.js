96263112c834299091fd847ece6a114cc2765dc3
Everett Grethel <everett.grethel@gmail.com>
2021-02-26 17:09:49 -0500

    fixed firebase

--- shoppinglist/js/main.js
+++ shoppinglist/js/main.js
@@ -13,9 +13,11 @@ class Item {
 }
 
 let shoppingList = [];
+let archiveList = [];
 let rawData;
 let instances;
 
+//get saved shopping list
 db.collection('list').get().then((snapshot) => {
     snapshot.docs.forEach(doc => {
         rawData = snapshot.docs;
@@ -27,6 +29,18 @@ db.collection('list').get().then((snapshot) => {
         shoppingList.push(item);
     });
     displayShoppingList();
+});
+
+//get saved completed list history
+db.collection('archive').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+        let item = new Item();
+        item.id = doc.id;
+        item.name = doc.data().name;
+        item.aisle = doc.data().aisle;
+        item.quantity = doc.data().quantity;
+        archiveList.push(item);
+    });
 });
 
 let onOpen = () => {
@@ -45,20 +59,62 @@ let onClose = () => {
         aisle: item.aisle,
         quantity: item.quantity
     })
-    console.log(item);
 }
+
+let addToArchive = (item) => {
+    archiveList.push(item);
+
+    db.collection('archive').add({
+        name: item.name,
+        aisle: item.aisle,
+        quantity: item.quantity
+    })
+}
+
+function displayArchive() {
+    console.log("display archive!");
+    let list = document.getElementById("archiveList");
+    list.innerText = "";
+
+    let outer_div = document.createElement("div");
+    let header = document.createElement("h2");
+    header.innerHTML = "List History";
+    outer_div.appendChild(header);
+
+    archiveList.forEach(item => {
+        let div = document.createElement("div");
+        div.classList.add("item_archived");
+
+        let h2 = document.createElement("h2");
+        let name = document.createTextNode(item.name);
+        h2.appendChild(name)
+        div.appendChild(h2);
+
+        let p_aisle = document.createElement("p");
+        let aisle = document.createTextNode("Aisle: " + item.aisle);
+        p_aisle.appendChild(aisle);
+        div.appendChild(p_aisle);
+
+        let p_quantity = document.createElement("p");
+        let quantity = document.createTextNode("Quantity: " + item.quantity);
+        p_quantity.appendChild(quantity);
+        div.appendChild(p_quantity);
+        
+        outer_div.append(div);
+
+        list.appendChild(outer_div);
+    });
+  }
 
 function displayShoppingList() {
     console.log("display shopping list!");
-    // Find the class roster unordered list
     let list = document.getElementById("shoppingList");
-    // Clear out the existing list.
     list.innerText = "";
-    // For each classmate in the roster, add a list item.
     shoppingList.forEach(item => {
         let outer_div = document.createElement("div");
         let div = document.createElement("div");
         div.classList.add("item");
+        outer_div.classList.add("outer_div");
 
         let h2 = document.createElement("h2");
         let name = document.createTextNode(item.name);
@@ -92,15 +148,21 @@ function displayShoppingList() {
         form_checkbox.appendChild(checkbox)
         let label = document.createElement("label");
         let input = document.createElement("input");
+        let span = document.createElement("span");
         input.setAttribute("type", "checkbox");
+        input.addEventListener("click", addToArchive(item));
+        label.appendChild(input);
+        label.appendChild(span);
         checkbox.appendChild(label);
-        checkbox.appendChild(input);
         outer_div.append(form_checkbox);
         outer_div.append(div);
 
         list.appendChild(outer_div);
     });
   }
+
+let archiveBtn = document.getElementById("archive-button");
+archiveBtn.addEventListener('click', displayArchive);
 
 document.addEventListener('DOMContentLoaded', function() {
     var modal = document.querySelectorAll('.modal');
@@ -112,6 +174,7 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
 //This function checks whether another user has made any changes and updates locally
+//Firebase does not permit this request frequency
 /*function checkDatabase() {
     db.collection('list').get().then((snapshot) => {
         shoppingList = [];
